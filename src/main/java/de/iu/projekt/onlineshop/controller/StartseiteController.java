edb401b37d7b9d723782b403d67aff48b1db0df4
Jakub Ballo <jakub.ballo@gmail.com>
2026-09-07 20:40:04 +0200

    Display latest 6 products on homepage
    Inject ProduktRepository into StartseiteController, fetch the 6 most recent products (findTop6ByOrderByIdDesc) and add them to the model as 'produktListe'. Add corresponding repository method signature. Update startseite.html to render the newest products as bootstrap cards (manufacturer, model, price, link). Adds required imports and Model parameter.

--- src/main/java/de/iu/projekt/onlineshop/controller/StartseiteController.java
+++ src/main/java/de/iu/projekt/onlineshop/controller/StartseiteController.java
@@ -1,14 +1,29 @@
 package de.iu.projekt.onlineshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import de.iu.projekt.onlineshop.model.Produkt;
+import de.iu.projekt.onlineshop.repository.ProduktRepository;
 
 @Controller
 public class StartseiteController {
 	
+	private final ProduktRepository produktRepository;
+	
+	public StartseiteController (ProduktRepository produktRepository) {
+		this.produktRepository = produktRepository;
+	}
+	
 	//Startseite 
 	@GetMapping("/")
-	public String startseite() {
+	public String startseite(Model model) {
+		List <Produkt> produktListe = produktRepository.findTop6ByOrderByIdDesc();
+		model.addAttribute("produktListe", produktListe);
+		
 		return "startseite";
 	}
 }
--- src/main/java/de/iu/projekt/onlineshop/repository/ProduktRepository.java
+++ src/main/java/de/iu/projekt/onlineshop/repository/ProduktRepository.java
@@ -1,5 +1,7 @@
 package de.iu.projekt.onlineshop.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +15,7 @@ public interface ProduktRepository  extends JpaRepository<Produkt, Long>{
 	//Alle produkte nach Kategorie ID zählen
 	long countByKategorieId(Long kategorieId);
 	
+	//Top 6 Items by Order
+	List<Produkt> findTop6ByOrderByIdDesc();
 	
 }
